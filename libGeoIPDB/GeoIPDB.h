c7b5686aa123bffdb4e3962a76f8682e1b48e1b5
Boris Zentner <bzentner@maxmind.com>
2013-01-02 15:45:48 +0100

    Rename GeoIPDB to IPDB.

--- libGeoIPDB/GeoIPDB.h
+++ libGeoIPDB/GeoIPDB.h
@@ -1,5 +1,5 @@
-#ifndef GEOIPDB_H
-#define GEOIPDB_H
+#ifndef IPDB_H
+#define IPDB_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -16,33 +16,33 @@ extern "C" {
 //#include <sys/types.h> /* for fstat */
 //#include <sys/stat.h>   /* for fstat */
 
-# define GEOIPDB_DTYPE_EXT (0)
-# define GEOIPDB_DTYPE_PTR (1)
-# define GEOIPDB_DTYPE_UTF8_STRING (2)
-# define GEOIPDB_DTYPE_DOUBLE (3)
-# define GEOIPDB_DTYPE_BYTES (4)
-# define GEOIPDB_DTYPE_UINT16 (5)
-# define GEOIPDB_DTYPE_UINT32 (6)
-# define GEOIPDB_DTYPE_HASH (7)
-# define GEOIPDB_DTYPE_INT32 (8)
-# define GEOIPDB_DTYPE_UINT64 (9)
-# define GEOIPDB_DTYPE_UINT128 (10)
-# define GEOIPDB_DTYPE_CONTAINER (11)
-# define GEOIPDB_DTYPE_END_MARKER (12)
+# define IPDB_DTYPE_EXT (0)
+# define IPDB_DTYPE_PTR (1)
+# define IPDB_DTYPE_UTF8_STRING (2)
+# define IPDB_DTYPE_DOUBLE (3)
+# define IPDB_DTYPE_BYTES (4)
+# define IPDB_DTYPE_UINT16 (5)
+# define IPDB_DTYPE_UINT32 (6)
+# define IPDB_DTYPE_HASH (7)
+# define IPDB_DTYPE_INT32 (8)
+# define IPDB_DTYPE_UINT64 (9)
+# define IPDB_DTYPE_UINT128 (10)
+# define IPDB_DTYPE_CONTAINER (11)
+# define IPDB_DTYPE_END_MARKER (12)
 
 /* GEOIPDB flags */
-#define GEOIPDB_MODE_STANDARD (1)
-#define GEOIPDB_MODE_MEMORY_CACHE (2)
-#define GEOIPDB_MODE_MASK (7)
+#define IPDB_MODE_STANDARD (1)
+#define IPDB_MODE_MEMORY_CACHE (2)
+#define IPDB_MODE_MASK (7)
 
 /* GEOIPDB err codes */
-#define GEOIPDB_SUCCESS (0)
-#define GEOIPDB_OPENFILEERROR (-1)
-#define GEOIPDB_CORRUPTDATABASE (-2)
-#define GEOIPDB_INVALIDDATABASE (-3)
-#define GEOIPDB_IOERROR (-4)
-#define GEOIPDB_OUTOFMEMORY (-5)
-	typedef struct GeoIPDB {
+#define IPDB_SUCCESS (0)
+#define IPDB_OPENFILEERROR (-1)
+#define IPDB_CORRUPTDATABASE (-2)
+#define IPDB_INVALIDDATABASE (-3)
+#define IPDB_IOERROR (-4)
+#define IPDB_OUTOFMEMORY (-5)
+	typedef struct IPDB {
 		uint32_t flags;
 		int fd;
 		const unsigned char *file_in_mem_ptr;
@@ -54,25 +54,25 @@ extern "C" {
 		int depth;
 		int segments;
 		const unsigned char *dataptr;
-	} GeoIPDB;
+	} IPDB;
 
-	typedef struct GeoIPDB_entry_s {
+	typedef struct IPDB_entry_s {
 		GEOIPDB *gi;
 		void *sptr;	/* usually pointer to the struct */
-	} GeoIPDB_entry_s;
+	} IPDB_entry_s;
 
 	typedef struct {
-		GeoIPDB_entry_s entry;
+		IPDB_entry_s entry;
 		int netmask;
-	} GeoIPDB_root_entry_s;
+	} IPDB_root_entry_s;
 
 
 #if 0
-	struct GeoIPDB_Decode_Value {
+	struct IPDB_Decode_Value {
 		SV *sv;
 		int new_offset;
 	};
-	struct GeoIPDB_Decode_Key {
+	struct IPDB_Decode_Key {
 		const char *ptr;
 		int size;
 		int new_offset;
@@ -85,4 +85,4 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
-#endif				/* GEOIPDB_H */
+#endif				/* IPDB_H */
--- libGeoIPDB/GeoIPDB_Core.c
+++ libGeoIPDB/GeoIPDB_Core.c
@@ -1,4 +1,4 @@
-#include "GeoIPDB.h"
+#include "IPDB.h"
 #include <sys/stat.h>
 #include <arpa/inet.h>
 #include <string.h>
@@ -57,39 +57,39 @@ static int _read(int fd, uint8_t * buffer, ssize_t to_read, off_t offset)
 	while (to_read > 0) {
 		ssize_t have_read = pread(fd, buffer, to_read, offset);
 		if (have_read <= 0)
-			return GEOIPDB_IOERROR;
+			return IPDB_IOERROR;
 		to_read -= have_read;
 		if (to_read == 0)
 			break;
 		offset += have_read;
 		buffer += have_read;
 	}
-	return GEOIPDB_SUCCESS;
+	return IPDB_SUCCESS;
 }
 
 static int
-_fddecode_key(GeoIPDB * gi, int offset, struct GeoIPDB_Decode_Key *ret_key)
+_fddecode_key(IPDB * gi, int offset, struct IPDB_Decode_Key *ret_key)
 {
 	const int segments = gi->segments * gi->recbits * 2 / 8;;
 	uint8_t ctrl;
 	int type;
 	uint8_t b[4];
 	int fd = gi->fd;
-	if (_read(fd, &ctrl, 1, segments + offset++) != GEOIPDB_SUCCESS)
-		return GEOIPDB_IOERROR;
+	if (_read(fd, &ctrl, 1, segments + offset++) != IPDB_SUCCESS)
+		return IPDB_IOERROR;
 	type = (ctrl >> 5) & 7;
-	if (type == GEOIPDB_DTYPE_EXT) {
-		if (_read(fd, &b[0], 1, segments + offset++) != GEOIPDB_SUCCESS)
-			return GEOIPDB_IOERROR;
+	if (type == IPDB_DTYPE_EXT) {
+		if (_read(fd, &b[0], 1, segments + offset++) != IPDB_SUCCESS)
+			return IPDB_IOERROR;
 		type = 8 + b[0];
 	}
 
-	if (type == GEOIPDB_DTYPE_PTR) {
+	if (type == IPDB_DTYPE_PTR) {
 		int psize = (ctrl >> 3) & 3;
 		int new_offset;
 		if (_read(fd, &b[0], psize + 1, segments + offset) !=
-		    GEOIPDB_SUCCESS)
-			return GEOIPDB_IOERROR;
+		    IPDB_SUCCESS)
+			return IPDB_IOERROR;
 		switch (psize) {
 		case 0:
 			new_offset = (ctrl & 7) * 256 + b[0];
@@ -108,28 +108,28 @@ _fddecode_key(GeoIPDB * gi, int offset, struct GeoIPDB_Decode_Key *ret_key)
 			new_offset = _get_uint32(b);
 			break;
 		}
-		if (_fddecode_key(gi, new_offset, ret_key) != GEOIPDB_SUCCESS)
-			return GEOIPDB_IOERROR;
+		if (_fddecode_key(gi, new_offset, ret_key) != IPDB_SUCCESS)
+			return IPDB_IOERROR;
 		ret_key->new_offset = offset + psize + 1;
-		return GEOIPDB_SUCCESS;
+		return IPDB_SUCCESS;
 	}
 
 	int size = ctrl & 31;
 	switch (size) {
 	case 29:
-		if (_read(fd, &b[0], 1, segments + offset++) != GEOIPDB_SUCCESS)
-			return GEOIPDB_IOERROR;
+		if (_read(fd, &b[0], 1, segments + offset++) != IPDB_SUCCESS)
+			return IPDB_IOERROR;
 		size = 29 + b[0];
 		break;
 	case 30:
-		if (_read(fd, &b[0], 2, segments + offset) != GEOIPDB_SUCCESS)
-			return GEOIPDB_IOERROR;
+		if (_read(fd, &b[0], 2, segments + offset) != IPDB_SUCCESS)
+			return IPDB_IOERROR;
 		size = 285 + b[0] * 256 + b[1];
 		offset += 2;
 		break;
 	case 31:
-		if (_read(fd, &b[0], 3, segments + offset) != GEOIPDB_SUCCESS)
-			return GEOIPDB_IOERROR;
+		if (_read(fd, &b[0], 3, segments + offset) != IPDB_SUCCESS)
+			return IPDB_IOERROR;
 		size = 65821 + _get_uint24(b);
 		offset += 3;
 	default:
@@ -140,18 +140,18 @@ _fddecode_key(GeoIPDB * gi, int offset, struct GeoIPDB_Decode_Key *ret_key)
 		ret_key->ptr = NULL;
 		ret_key->size = 0;
 		ret_key->new_offset = offset;
-		return GEOIPDB_SUCCESS;
+		return IPDB_SUCCESS;
 	}
 
 	ret_key->ptr = (void *)0 + segments + offset;
 	ret_key->size = size;
 	ret_key->new_offset = offset + size;
-	return GEOIPDB_SUCCESS;
+	return IPDB_SUCCESS;
 }
 
 #define GEOIP_CHKBIT_V6(bit,ptr) ((ptr)[((127UL - (bit)) >> 3)] & (1UL << (~(127UL - (bit)) & 7)))
 
-void GeoIPDB_free_all(GeoIPDB * gi)
+void IPDB_free_all(IPDB * gi)
 {
 	if (gi) {
 		if (gi->fd >= 0)
@@ -163,7 +163,7 @@ void GeoIPDB_free_all(GeoIPDB * gi)
 }
 
 static int
-_fdlookup_by_ipnum(GeoIPDB * gi, uint32_t ipnum, struct GeoIPDB_Lookup *result)
+_fdlookup_by_ipnum(IPDB * gi, uint32_t ipnum, struct IPDB_Lookup *result)
 {
 	int segments = gi->segments;
 	off_t offset = 0;
@@ -179,13 +179,13 @@ _fdlookup_by_ipnum(GeoIPDB * gi, uint32_t ipnum, struct GeoIPDB_Lookup *result)
 			if (_read
 			    (fd, &b[0], 3,
 			     offset * rl + ((ipnum & mask) ? 3 : 0)) !=
-			    GEOIPDB_SUCCESS)
-				return GEOIPDB_IOERROR;
+			    IPDB_SUCCESS)
+				return IPDB_IOERROR;
 			offset = _get_uint24(b);
 			if (offset >= segments) {
 				result->netmask = 32 - depth;
 				result->ptr = offset - segments;
-				return GEOIPDB_SUCCESS;
+				return IPDB_SUCCESS;
 			}
 		}
 	} else if (rl == 7) {
@@ -193,14 +193,14 @@ _fdlookup_by_ipnum(GeoIPDB * gi, uint32_t ipnum, struct GeoIPDB_Lookup *result)
 			byte_offset = offset * rl;
 			if (ipnum & mask) {
 				if (_read(fd, &b[0], 4, byte_offset + 3) !=
-				    GEOIPDB_SUCCESS)
-					return GEOIPDB_IOERROR;
+				    IPDB_SUCCESS)
+					return IPDB_IOERROR;
 				offset = _get_uint32(b);
 				offset &= 0xfffffff;
 			} else {
 				if (_read(fd, &b[0], 4, byte_offset) !=
-				    GEOIPDB_SUCCESS)
-					return GEOIPDB_IOERROR;
+				    IPDB_SUCCESS)
+					return IPDB_IOERROR;
 				offset =
 				    b[0] * 65536 + b[1] * 256 + b[2] +
 				    ((b[3] & 0xf0) << 20);
@@ -208,7 +208,7 @@ _fdlookup_by_ipnum(GeoIPDB * gi, uint32_t ipnum, struct GeoIPDB_Lookup *result)
 			if (offset >= segments) {
 				result->netmask = 32 - depth;
 				result->ptr = offset - segments;
-				return GEOIPDB_SUCCESS;
+				return IPDB_SUCCESS;
 			}
 		}
 	} else if (rl == 8) {
@@ -216,23 +216,23 @@ _fdlookup_by_ipnum(GeoIPDB * gi, uint32_t ipnum, struct GeoIPDB_Lookup *result)
 			if (_read
 			    (fd, &b[0], 4,
 			     offset * rl + ((ipnum & mask) ? 4 : 0)) !=
-			    GEOIPDB_SUCCESS)
-				return GEOIPDB_IOERROR;
+			    IPDB_SUCCESS)
+				return IPDB_IOERROR;
 			offset = _get_uint32(b);
 			if (offset >= segments) {
 				result->netmask = 32 - depth;
 				result->ptr = offset - segments;
-				return GEOIPDB_SUCCESS;
+				return IPDB_SUCCESS;
 			}
 		}
 	}
 	//uhhh should never happen !
-	return GEOIPDB_CORRUPTDATABASE;
+	return IPDB_CORRUPTDATABASE;
 }
 
 static int
-_fdlookup_by_ipnum_128(GeoIPDB * gi, struct in6_addr ipnum,
-		       struct GeoIPDB_Lookup *result)
+_fdlookup_by_ipnum_128(IPDB * gi, struct in6_addr ipnum,
+		       struct IPDB_Lookup *result)
 {
 	int segments = gi->segments;
 	int offset = 0;
@@ -247,13 +247,13 @@ _fdlookup_by_ipnum_128(GeoIPDB * gi, struct in6_addr ipnum,
 			byte_offset = offset * rl;
 			if (GEOIP_CHKBIT_V6(depth, (uint8_t *) & ipnum))
 				byte_offset += 3;
-			if (_read(fd, &b[0], 3, byte_offset) != GEOIPDB_SUCCESS)
-				return GEOIPDB_IOERROR;
+			if (_read(fd, &b[0], 3, byte_offset) != IPDB_SUCCESS)
+				return IPDB_IOERROR;
 			offset = _get_uint24(b);
 			if (offset >= segments) {
 				result->netmask = 128 - depth;
 				result->ptr = offset - segments;
-				return GEOIPDB_SUCCESS;
+				return IPDB_SUCCESS;
 			}
 		}
 	} else if (rl == 7) {
@@ -262,15 +262,15 @@ _fdlookup_by_ipnum_128(GeoIPDB * gi, struct in6_addr ipnum,
 			if (GEOIP_CHKBIT_V6(depth, (uint8_t *) & ipnum)) {
 				byte_offset += 3;
 				if (_read(fd, &b[0], 4, byte_offset) !=
-				    GEOIPDB_SUCCESS)
-					return GEOIPDB_IOERROR;
+				    IPDB_SUCCESS)
+					return IPDB_IOERROR;
 				offset = _get_uint32(b);
 				offset &= 0xfffffff;
 			} else {
 
 				if (_read(fd, &b[0], 4, byte_offset) !=
-				    GEOIPDB_SUCCESS)
-					return GEOIPDB_IOERROR;
+				    IPDB_SUCCESS)
+					return IPDB_IOERROR;
 				offset =
 				    b[0] * 65536 + b[1] * 256 + b[2] +
 				    ((b[3] & 0xf0) << 20);
@@ -278,7 +278,7 @@ _fdlookup_by_ipnum_128(GeoIPDB * gi, struct in6_addr ipnum,
 			if (offset >= segments) {
 				result->netmask = 128 - depth;
 				result->ptr = offset - segments;
-				return GEOIPDB_SUCCESS;
+				return IPDB_SUCCESS;
 			}
 		}
 	} else if (rl == 8) {
@@ -286,23 +286,23 @@ _fdlookup_by_ipnum_128(GeoIPDB * gi, struct in6_addr ipnum,
 			byte_offset = offset * rl;
 			if (GEOIP_CHKBIT_V6(depth, (uint8_t *) & ipnum))
 				byte_offset += 4;
-			if (_read(fd, &b[0], 4, byte_offset) != GEOIPDB_SUCCESS)
-				return GEOIPDB_IOERROR;
+			if (_read(fd, &b[0], 4, byte_offset) != IPDB_SUCCESS)
+				return IPDB_IOERROR;
 			offset = _get_uint32(b);
 			if (offset >= segments) {
 				result->netmask = 128 - depth;
 				result->ptr = offset - segments;
-				return GEOIPDB_SUCCESS;
+				return IPDB_SUCCESS;
 			}
 		}
 	}
 	//uhhh should never happen !
-	return GEOIPDB_CORRUPTDATABASE;
+	return IPDB_CORRUPTDATABASE;
 }
 
 static int
-_lookup_by_ipnum_128(GeoIPDB * gi, struct in6_addr ipnum,
-		     struct GeoIPDB_Lookup *result)
+_lookup_by_ipnum_128(IPDB * gi, struct in6_addr ipnum,
+		     struct IPDB_Lookup *result)
 {
 	int segments = gi->segments;
 	int offset = 0;
@@ -320,7 +320,7 @@ _lookup_by_ipnum_128(GeoIPDB * gi, struct in6_addr ipnum,
 			if (offset >= segments) {
 				result->netmask = 128 - depth;
 				result->ptr = offset - segments;
-				return GEOIPDB_SUCCESS;
+				return IPDB_SUCCESS;
 			}
 		}
 	} else if (rl == 7) {
@@ -339,7 +339,7 @@ _lookup_by_ipnum_128(GeoIPDB * gi, struct in6_addr ipnum,
 			if (offset >= segments) {
 				result->netmask = 128 - depth;
 				result->ptr = offset - segments;
-				return GEOIPDB_SUCCESS;
+				return IPDB_SUCCESS;
 			}
 		}
 	} else if (rl == 8) {
@@ -351,16 +351,16 @@ _lookup_by_ipnum_128(GeoIPDB * gi, struct in6_addr ipnum,
 			if (offset >= segments) {
 				result->netmask = 128 - depth;
 				result->ptr = offset - segments;
-				return GEOIPDB_SUCCESS;
+				return IPDB_SUCCESS;
 			}
 		}
 	}
 	//uhhh should never happen !
-	return GEOIPDB_CORRUPTDATABASE;
+	return IPDB_CORRUPTDATABASE;
 }
 
 static int
-_lookup_by_ipnum(GeoIPDB * gi, uint32_t ipnum, struct GeoIPDB_Lookup *res)
+_lookup_by_ipnum(IPDB * gi, uint32_t ipnum, struct IPDB_Lookup *res)
 {
 	int segments = gi->segments;
 	int offset = 0;
@@ -378,7 +378,7 @@ _lookup_by_ipnum(GeoIPDB * gi, uint32_t ipnum, struct GeoIPDB_Lookup *res)
 			if (offset >= segments) {
 				res->netmask = 32 - depth;
 				res->ptr = offset - segments;
-				return GEOIPDB_SUCCESS;
+				return IPDB_SUCCESS;
 			}
 			mask >>= 1;
 		}
@@ -397,7 +397,7 @@ _lookup_by_ipnum(GeoIPDB * gi, uint32_t ipnum, struct GeoIPDB_Lookup *res)
 			if (offset >= segments) {
 				res->netmask = 32 - depth;
 				res->ptr = offset - segments;
-				return GEOIPDB_SUCCESS;
+				return IPDB_SUCCESS;
 			}
 			mask >>= 1;
 		}
@@ -410,17 +410,17 @@ _lookup_by_ipnum(GeoIPDB * gi, uint32_t ipnum, struct GeoIPDB_Lookup *res)
 			if (offset >= segments) {
 				res->netmask = 32 - depth;
 				res->ptr = offset - segments;
-				return GEOIPDB_SUCCESS;
+				return IPDB_SUCCESS;
 			}
 			mask >>= 1;
 		}
 	}
 	//uhhh should never happen !
-	return GEOIPDB_CORRUPTDATABASE;
+	return IPDB_CORRUPTDATABASE;
 }
 
 static void
-_decode_key(GeoIPDB * gi, int offset, struct GeoIPDB_Decode_Key *ret_key)
+_decode_key(IPDB * gi, int offset, struct IPDB_Decode_Key *ret_key)
 {
 	//int           segments = gi->segments;
 	const int segments = 0;
@@ -428,11 +428,11 @@ _decode_key(GeoIPDB * gi, int offset, struct GeoIPDB_Decode_Key *ret_key)
 	uint8_t ctrl, type;
 	ctrl = mem[segments + offset++];
 	type = (ctrl >> 5) & 7;
-	if (type == GEOIPDB_DTYPE_EXT) {
+	if (type == IPDB_DTYPE_EXT) {
 		type = 8 + mem[segments + offset++];
 	}
 
-	if (type == GEOIPDB_DTYPE_PTR) {
+	if (type == IPDB_DTYPE_PTR) {
 		int psize = (ctrl >> 3) & 3;
 		int new_offset;
 		switch (psize) {
@@ -488,7 +488,7 @@ _decode_key(GeoIPDB * gi, int offset, struct GeoIPDB_Decode_Key *ret_key)
 	return;
 }
 
-static int _init(GeoIPDB * gi, char *fname, uint32_t flags)
+static int _init(IPDB * gi, char *fname, uint32_t flags)
 {
 	struct stat s;
 	int fd;
@@ -498,10 +498,10 @@ static int _init(GeoIPDB * gi, char *fname, uint32_t flags)
 	off_t offset;
 	gi->fd = fd = open(fname, O_RDONLY);
 	if (fd < 0)
-		return GEOIPDB_OPENFILEERROR;
+		return IPDB_OPENFILEERROR;
 	fstat(fd, &s);
 	gi->flags = flags;
-	if ((flags & GEOIPDB_MODE_MASK) == GEOIPDB_MODE_MEMORY_CACHE) {
+	if ((flags & IPDB_MODE_MASK) == IPDB_MODE_MEMORY_CACHE) {
 		gi->fd = -1;
 		size = s.st_size;
 		offset = 0;
@@ -512,14 +512,14 @@ static int _init(GeoIPDB * gi, char *fname, uint32_t flags)
 	}
 	ptr = malloc(size);
 	if (ptr == NULL)
-		return GEOIPDB_INVALIDDATABASE;
+		return IPDB_INVALIDDATABASE;
 
 	iread = pread(fd, ptr, size, offset);
 
 	const uint8_t *p = memmem(ptr, size, "\xab\xcd\xefMaxMind.com", 14);
 	if (p == NULL) {
 		free(ptr);
-		return GEOIPDB_INVALIDDATABASE;
+		return IPDB_INVALIDDATABASE;
 	}
 	p += 14;
 	gi->file_format = p[0] * 256 + p[1];
@@ -529,7 +529,7 @@ static int _init(GeoIPDB * gi, char *fname, uint32_t flags)
 	gi->minor_database_type = p[6] * 256 + p[7];
 	gi->segments = p[8] * 16777216 + p[9] * 65536 + p[10] * 256 + p[11];
 
-	if ((flags & GEOIPDB_MODE_MASK) == GEOIPDB_MODE_MEMORY_CACHE) {
+	if ((flags & IPDB_MODE_MASK) == IPDB_MODE_MEMORY_CACHE) {
 		gi->file_in_mem_ptr = ptr;
 		gi->dataptr =
 		    gi->file_in_mem_ptr + gi->segments * gi->recbits * 2 / 8;
@@ -540,14 +540,14 @@ static int _init(GeoIPDB * gi, char *fname, uint32_t flags)
 		    (const uint8_t *)0 + (gi->segments * gi->recbits * 2 / 8);
 		free(ptr);
 	}
-	return GEOIPDB_SUCCESS;
+	return IPDB_SUCCESS;
 }
 
-GeoIPDB *GeoIPDB_open(char *fname, uint32_t flags)
+IPDB *IPDB_open(char *fname, uint32_t flags)
 {
-	GeoIPDB *gi = calloc(1, sizeof(GeoIPDB));
-	if (GEOIPDB_SUCCESS != _init(gi, fname, flags)) {
-		GeoIPDB_free_all(gi);
+	IPDB *gi = calloc(1, sizeof(IPDB));
+	if (IPDB_SUCCESS != _init(gi, fname, flags)) {
+		IPDB_free_all(gi);
 		return NULL;
 	}
 	return gi;
