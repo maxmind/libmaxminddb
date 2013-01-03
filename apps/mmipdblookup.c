c1df0e1d0bff5217e9aaade1826692c1696b8087
Boris Zentner <bzentner@maxmind.com>
2013-01-03 17:18:25 +0100

    Add some options to the dummy lookuptool mmipdblookup

--- apps/mmipdblookup.c
+++ apps/mmipdblookup.c
@@ -1,8 +1,42 @@
 #include "MMIPDB.h"
 #include "MMIPDB_Helper.h"
-#include "getopt.h"
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 /* dummy content */
-int main(void)
+
+void usage(char *prg)
 {
+    fprintf(stderr, "Usage: %s -f database addr\n", prg);
+    exit(1);
+}
+
+int main(int argc, char *const argv[])
+{
+    int verbose = 0;
+    int character;
+    char *fname = NULL;
+
+    while ((character = getopt(argc, argv, "vf:")) != -1) {
+        switch (character) {
+        case 'v':
+            verbose = 1;
+            break;
+        case 'f':
+            fname = strdup(optarg);
+            break;
+        default:
+        case '?':
+            usage(argv[0]);
+        }
+    }
+    argc -= optind;
+    argv += optind;
+
+    //MMIPDB_s ipdb = MMIPDB_open( fname );
+    if ( fname )
+        free(fname);
+
     return (0);
 }
