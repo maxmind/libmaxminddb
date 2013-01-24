5749806f8fdb01b1e8f49e657128fbe9442ca7b0
Boris Zentner <bzentner@maxmind.com>
2013-01-24 21:02:10 +0100

    Add test version_t

---
+++ t/version_t.c
@@ -0,0 +1,15 @@
+#include "MMDB.h"
+#include "tap.h"
+#include <sys/stat.h>
+#include <string.h>
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+int main(void)
+{
+    const char *version = MMDB_lib_version();
+    ok(version != NULL, "MMDB_lib_version exists");
+    ok(strcmp(version, PACKAGE_VERSION) == 0, "version is " PACKAGE_VERSION);
+    done_testing();
+}
