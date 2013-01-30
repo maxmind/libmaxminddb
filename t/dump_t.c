267a85be072e08a9a21f689d2ae4aa0be79b3f85
Boris Zentner <bzentner@maxmind.com>
2013-01-30 13:58:22 +0100

    Add dump_t.c example

---
+++ t/dump_t.c
@@ -0,0 +1,42 @@
+#include "MMDB.h"
+#include "tap.h"
+#include <sys/stat.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+uint32_t ip_to_num(char *ipstr)
+{
+    struct in_addr ip;
+    if (ipstr == NULL || 1 != inet_pton(AF_INET, ipstr, &ip))
+        return 0;
+    return htonl(ip.s_addr);
+}
+
+int main(void)
+{
+    char *fname = "./data/test-database.dat";
+    struct stat sstat;
+    int err = stat(fname, &sstat);
+    ok(err == 0, "%s exists", fname);
+
+    MMDB_s *mmdb = MMDB_open(fname, MMDB_MODE_MEMORY_CACHE);
+    ok(mmdb != NULL, "MMDB_open successful");
+    if (mmdb) {
+
+        MMDB_root_entry_s root = {.entry.mmdb = mmdb };
+        char *ipstr = "24.24.24.24";
+        uint32_t ipnum = ip_to_num(ipstr);
+        err = MMDB_lookup_by_ipnum(ipnum, &root);
+        ok(err == MMDB_SUCCESS, "Search for %s SUCCESSFUL", ipstr);
+        ok(root.entry.offset > 0, "Found something %s good", ipstr);
+#if defined BROKEN_SEARCHTREE
+        root.entry.offset -= mmdb->node_count;
+#endif
+        MMDB_decode_all_s *decode_all = calloc(1, sizeof(MMDB_decode_all_s));
+        int err = MMDB_get_tree(&root.entry, &decode_all);
+
+        if (decode_all != NULL)
+            MMDB_dump(decode_all, 0);
+    }
+    done_testing();
+}
