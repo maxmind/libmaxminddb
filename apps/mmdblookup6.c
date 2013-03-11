64035ed54e5cf1f4a30c4b9b07cf21366dae2c01
Boris Zentner <bzentner@maxmind.com>
2013-03-11 15:49:16 +0100

    Add mmdblookup6 to lookup v6 databases

---
+++ apps/mmdblookup6.c
@@ -0,0 +1,115 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "MMDB.h"
+#include "MMDB_Helper.h"
+#include "getopt.h"
+#include <assert.h>
+
+/* dummy content */
+
+#define die(...) do { \
+    fprintf(stderr, __VA_ARGS__); \
+    exit(1); \
+} while(0)
+
+void usage(char *prg)
+{
+    die("Usage: %s -f database addr\n", prg);
+}
+
+int main(int argc, char *const argv[])
+{
+    int verbose = 0;
+    int character;
+    char *fname = NULL;
+
+    while ((character = getopt(argc, argv, "vf:")) != -1) {
+        switch (character) {
+        case 'v':
+            verbose = 1;
+            break;
+        case 'f':
+            fname = strdup(optarg);
+            break;
+        default:
+        case '?':
+            usage(argv[0]);
+        }
+    }
+    argc -= optind;
+    argv += optind;
+
+    if (!fname) {
+        fname = strdup("/usr/local/share/GeoIP2/city-v6.db");
+    }
+
+    assert(fname != NULL);
+
+    MMDB_s *mmdb = MMDB_open(fname, MMDB_MODE_MEMORY_CACHE);
+
+    if (!mmdb)
+        die("Can't open %s\n", fname);
+
+    free(fname);
+
+    char *ipstr = argv[0];
+    struct in6_addr ip;
+    if (ipstr == NULL || 1 != addr6_to_num(ipstr, &ip))
+        die("Invalid IP\n");
+
+    if (verbose) {
+        MMDB_decode_all_s *decode_all = calloc(1, sizeof(MMDB_decode_all_s));
+        int err = MMDB_get_tree(&mmdb->meta, &decode_all);
+        assert(err == MMDB_SUCCESS);
+
+        if (decode_all != NULL)
+            MMDB_dump(decode_all, 0);
+        free(decode_all);
+    }
+
+    MMDB_root_entry_s root = {.entry.mmdb = mmdb };
+    //uint32_t ipnum = htonl(ip.s_addr);
+    int err = MMDB_lookup_by_ipnum_128(ip, &root);
+    if (err == MMDB_SUCCESS) {
+        double dlat, dlon;
+        char *city, *country, *region_name, code2[3];
+        if (root.entry.offset > 0) {
+            MMDB_return_s res_location;
+            MMDB_get_value(&root.entry, &res_location, "location", NULL);
+            // TODO handle failed search somehow.
+            MMDB_return_s lat, lon, field;
+            MMDB_entry_s location = {.mmdb = mmdb,.offset = res_location.offset
+            };
+            if (res_location.offset) {
+                MMDB_get_value(&location, &lat, "latitude", NULL);
+                MMDB_get_value(&location, &lon, "longitude", NULL);
+                dlat = lat.double_value;
+                dlon = lon.double_value;
+            } else {
+                dlon = dlat = 0;
+            }
+
+            //     printf( "%u %f %f\n", ipnum , dlat, dlon);
+
+            MMDB_return_s res;
+            MMDB_get_value(&root.entry, &res, "city", "name", "en", NULL);
+            city = bytesdup(&res);
+            MMDB_get_value(&root.entry, &res, "country", "name", "en", NULL);
+            country = bytesdup(&res);
+
+            printf("%s %f %f %s %s\n", ipstr, dlat, dlon, city == NULL ? "N/A" : city, country);
+
+            if (city)
+                free(city);
+            if (country)
+                free(country);
+
+        } else {
+            puts("Sorry, nothing found");       // not found
+        }
+    }
+
+    return (0);
+}
