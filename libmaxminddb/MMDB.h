2c2c3e3e6f9e2f685d448ecd2a1995dc12a09dbd
Dave Rolsky <autarch@urth.org>
2013-05-30 10:29:40 -0500

    Make all defines consistent

--- libmaxminddb/MMDB.h
+++ libmaxminddb/MMDB.h
@@ -16,23 +16,23 @@ extern "C" {
 // only th 32bit ptr contains all of them.
 #define BROKEN_PTR (0)
 
-# define MMDB_DTYPE_EXT (0)
-# define MMDB_DTYPE_PTR (1)
-# define MMDB_DTYPE_UTF8_STRING (2)
-# define MMDB_DTYPE_DOUBLE (3)
-# define MMDB_DTYPE_BYTES (4)
-# define MMDB_DTYPE_UINT16 (5)
-# define MMDB_DTYPE_UINT32 (6)
-# define MMDB_DTYPE_MAP (7)     /* HASH */
-# define MMDB_DTYPE_INT32 (8)
-# define MMDB_DTYPE_UINT64 (9)
-# define MMDB_DTYPE_UINT128 (10)
-# define MMDB_DTYPE_ARRAY (11)
-# define MMDB_DTYPE_CONTAINER (12)
-# define MMDB_DTYPE_END_MARKER (13)
-# define MMDB_DTYPE_BOOLEAN (14)
+#define MMDB_DTYPE_EXT (0)
+#define MMDB_DTYPE_PTR (1)
+#define MMDB_DTYPE_UTF8_STRING (2)
+#define MMDB_DTYPE_DOUBLE (3)
+#define MMDB_DTYPE_BYTES (4)
+#define MMDB_DTYPE_UINT16 (5)
+#define MMDB_DTYPE_UINT32 (6)
+#define MMDB_DTYPE_MAP (7)     /* HASH */
+#define MMDB_DTYPE_INT32 (8)
+#define MMDB_DTYPE_UINT64 (9)
+#define MMDB_DTYPE_UINT128 (10)
+#define MMDB_DTYPE_ARRAY (11)
+#define MMDB_DTYPE_CONTAINER (12)
+#define MMDB_DTYPE_END_MARKER (13)
+#define MMDB_DTYPE_BOOLEAN (14)
 
-# define MMDB_DTYPE_MAX (MMDB_DTYPE_END_MARKER)
+#define MMDB_DTYPE_MAX (MMDB_DTYPE_END_MARKER)
 
 #define MMDB_DATASECTION_NOOP_SIZE (16)
 
@@ -54,7 +54,7 @@ extern "C" {
 #define MMDB_FALSE (0)
 
 /* */
-# define MMDB_DEBUG (0)
+#define MMDB_DEBUG (0)
 
 #if MMDB_DEBUG
 #define MMDB_DBG_CARP(...) fprintf(stderr, __VA_ARGS__ );
