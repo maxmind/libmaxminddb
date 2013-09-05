9ed33073426b246eccc8c608cc4dc1f1dc09f447
Dave Rolsky <autarch@urth.org>
2013-09-05 13:37:24 -0500

    Fix memory leaks in no_map_get_value_t

--- t/no_map_get_value_t.c
+++ t/no_map_get_value_t.c
@@ -5,18 +5,23 @@ void run_tests(int mode, const char *mode_desc)
     const char *filename = "MaxMind-DB-string-value-entries.mmdb";
     const char *path = test_database_path(filename);
     MMDB_s *mmdb = open_ok(path, mode, mode_desc);
+    free((void *)path);
 
     const char *ip = "1.1.1.1";
     MMDB_lookup_result_s *result = lookup_ok(mmdb, ip, filename, mode_desc);
 
-    MMDB_entry_data_s *entry_data = malloc(sizeof(MMDB_entry_data_s));
-    int status = MMDB_get_value(&result->entry, entry_data, NULL);
+    MMDB_entry_data_s entry_data;
+    int status = MMDB_get_value(&result->entry, &entry_data, NULL);
+    free(result);
+
     cmp_ok(status, "==", MMDB_SUCCESS,
            "status for MMDB_get_value() is MMDB_SUCCESS");
-    ok(NULL != entry_data, "found a value when varargs list is just NULL");
-    cmp_ok(entry_data->type, "==", MMDB_DTYPE_UTF8_STRING,
+    ok(entry_data.offset, "found a value when varargs list is just NULL");
+    cmp_ok(entry_data.type, "==", MMDB_DTYPE_UTF8_STRING,
            "returned entry type is utf8_string");
-    free(entry_data);
+
+    MMDB_close(mmdb);
+    free(mmdb);
 }
 
 int main(void)
