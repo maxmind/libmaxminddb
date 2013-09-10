2dff8eb5fe640fa1bacc28966b0fc03e3e3d15d5
Dave Rolsky <autarch@urth.org>
2013-09-10 14:56:44 -0500

    Uncrustify appears to be idempotent - no need to run it twice on each file

--- dev-bin/uncrustify-all.sh
+++ dev-bin/uncrustify-all.sh
@@ -8,12 +8,10 @@ for dir in bin include src t; do
     c_files=`find $dir -maxdepth 1 -name '*.c'`
     if [ "$c_files" != "" ]; then
         $uncrustify $dir/*.c;
-        $uncrustify $dir/*.c;
     fi
     
     h_files=`find $dir -maxdepth 1 -name '*.h'`
     if [ "$h_files" != "" ]; then
-        $uncrustify $dir/*.h;
         $uncrustify $dir/*.h;
     fi
 done
