2b729042778b3109db16c9a600a47cbf854ce6a1
Dave Rolsky <autarch@urth.org>
2013-09-11 16:22:04 -0500

    Update valgrind-all which new tests and removed CLIs

--- dev-bin/valgrind-all.pl
+++ dev-bin/valgrind-all.pl
@@ -12,9 +12,11 @@ my $top_dir = "$Bin/..";
 my $output;
 
 my @tests = map { ["$top_dir/t/.libs/$_"] } qw(
+    lt-aget_value_t
     lt-basic_lookup_t
     lt-data_entry_list_t
     lt-data_types_t
+    lt-dump_t
     lt-metadata_t
     lt-no_map_get_value_t
     lt-version_t
@@ -22,12 +24,8 @@ my @tests = map { ["$top_dir/t/.libs/$_"] } qw(
 
 my @mmdblookup = (
     "$top_dir/bin/mmdblookup",
-    '-f', "$top_dir/maxmind-db/test-data/MaxMind-DB-test-decoder.mmdb",
-);
-
-my @mmdbdump = (
-    "$top_dir/bin/mmdbdump",
-    '-f', "$top_dir/maxmind-db/test-data/MaxMind-DB-test-decoder.mmdb",
+    '--file', "$top_dir/maxmind-db/test-data/MaxMind-DB-test-decoder.mmdb",
+    '--ip',
 );
 
 # We want IPv4 and IPv6 addresses - one of each that exists in the db and one
@@ -36,7 +34,6 @@ my @ips = ( '1.1.1.1', '10.0.0.0', 'abcd::', '0900::' );
 
 my @cmds = (
     ( map { [ @mmdblookup, $_ ] } @ips ),
-    ( map { [ @mmdbdump, $_ ] } @ips ),
     @tests,
 );
 
