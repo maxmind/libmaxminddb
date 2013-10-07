6d565c10bbb3ae0a3844f2aeed0ac1f603f46ef7
Dave Rolsky <autarch@urth.org>
2013-10-07 16:45:22 -0500

    Check aget_value with an array index other than 0

--- t/aget_value_t.c
+++ t/aget_value_t.c
@@ -11,16 +11,31 @@ void run_tests(int mode, const char *mode_desc)
     MMDB_lookup_result_s result =
         lookup_string_ok(mmdb, ip, filename, mode_desc);
 
-    MMDB_entry_data_s entry_data;
-    char *lookup_path[] = { "array", "0", NULL };
-    int status = MMDB_aget_value(&result.entry, &entry_data, lookup_path);
+    {
+        MMDB_entry_data_s entry_data;
+        char *lookup_path[] = { "array", "0", NULL };
+        int status = MMDB_aget_value(&result.entry, &entry_data, lookup_path);
 
-    cmp_ok(status, "==", MMDB_SUCCESS,
-           "status for MMDB_aget_value() is MMDB_SUCCESS");
-    ok(entry_data.has_data, "found a value with MMDB_aget_value");
-    cmp_ok(entry_data.type, "==", MMDB_DATA_TYPE_UINT32,
-           "returned entry type is uint32");
-    cmp_ok(entry_data.uint32, "==", 1, "entry value is 1");
+        cmp_ok(status, "==", MMDB_SUCCESS,
+               "status for MMDB_aget_value() is MMDB_SUCCESS");
+        ok(entry_data.has_data, "found a value with MMDB_aget_value - array[0]");
+        cmp_ok(entry_data.type, "==", MMDB_DATA_TYPE_UINT32,
+               "returned entry type is uint32 - array[0]");
+        cmp_ok(entry_data.uint32, "==", 1, "entry value is 1 - array[0]");
+    }
+
+    {
+        MMDB_entry_data_s entry_data;
+        char *lookup_path[] = { "array", "2", NULL };
+        int status = MMDB_aget_value(&result.entry, &entry_data, lookup_path);
+
+        cmp_ok(status, "==", MMDB_SUCCESS,
+               "status for MMDB_aget_value() is MMDB_SUCCESS - array[2]");
+        ok(entry_data.has_data, "found a value with MMDB_aget_value - array[2]");
+        cmp_ok(entry_data.type, "==", MMDB_DATA_TYPE_UINT32,
+               "returned entry type is uint32 - array[2]");
+        cmp_ok(entry_data.uint32, "==", 3, "entry value is 3 - array[2]");
+    }
 
     MMDB_close(mmdb);
     free(mmdb);
