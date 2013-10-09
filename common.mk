ec5e6edc28e933fe55ed27d0852e53e018adf483
Dave Rolsky <autarch@urth.org>
2013-10-09 12:26:05 -0500

    Add comment about gross autoconf bits

--- common.mk
+++ common.mk
@@ -1,3 +1,6 @@
+# This is really gross since we end up with "--std=gnu99 ... --std=c99" in the
+# flags. I can't figure out how to tell autoconf to _not_ use gnu99.
+
 if DEBUG
 AM_CFLAGS=-O0 -g -Wall -Wextra --std=c99
 else
