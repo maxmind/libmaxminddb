90a0c28868bb083ea03b5f6f4c6623f5215c256d
Dave Rolsky <autarch@urth.org>
2013-10-09 13:47:41 -0500

    Use a slightly less gross method of ensuring we get c99, not gnu99

--- common.mk
+++ common.mk
@@ -1,10 +1,7 @@
-# This is really gross since we end up with "--std=gnu99 ... --std=c99" in the
-# flags. I can't figure out how to tell autoconf to _not_ use gnu99.
-
 if DEBUG
-AM_CFLAGS=-O0 -g -Wall -Wextra --std=c99
+AM_CFLAGS=-O0 -g -Wall -Wextra
 else
-AM_CFLAGS=-O2 -g --std=c99
+AM_CFLAGS=-O2 -g
 endif
 
 AM_CPPFLAGS = -I$(top_srcdir)/include
