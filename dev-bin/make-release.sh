08538136fd3d6761a05131a0313138dd18cb0279
Dave Rolsky <autarch@urth.org>
2013-11-19 11:50:14 -0600

    Move docs to index.md and remove most files

--- dev-bin/make-release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-VERSION=$@
-
-git tag $VERSION
-git push --tags
--- dev-bin/maybe-fake-man.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -f man/man3/libmaxminddb.3 ]; then
-	true
-else
-	mkdir -p man/man3
-	touch man/man3/libmaxminddb.3
-fi
