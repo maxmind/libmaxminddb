3f59a4277ffd5b143bca5940ee642a3af5e84363
Gregory Oschwald <goschwald@maxmind.com>
2017-05-15 09:35:00 -0700

    Bumped version to 1.2.1

--- include/maxminddb.h
+++ include/maxminddb.h
@@ -20,7 +20,7 @@ extern "C" {
 #include <winsock2.h>
 #include <ws2tcpip.h>
 /* libmaxminddb package version from configure */
-#define PACKAGE_VERSION "1.2.0"
+#define PACKAGE_VERSION "1.2.1"
 
 typedef ADDRESS_FAMILY sa_family_t;
 
