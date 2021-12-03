047451d889788cac59c2507ccce9145723daf038
William Storey <wstorey@maxmind.com>
2021-12-02 16:14:34 -0800

    Clean up in C++ test

--- t/compile_c++_t.pl
+++ t/compile_c++_t.pl
@@ -28,7 +28,11 @@ int main(int argc, char *argv[])
 {
     const char *fname = "$test_db";
     MMDB_s mmdb;
-    return MMDB_open(fname, MMDB_MODE_MMAP, &mmdb);
+    if (MMDB_open(fname, MMDB_MODE_MMAP, &mmdb) != MMDB_SUCCESS) {
+        return 1;
+    }
+    MMDB_close(&mmdb);
+    return 0;
 }
 EOF
 
